853f3cf9f053b91fc00db87092d2286908f3a6f3
hasika-sandaruwan <hasikasandaruwan.info@gmail.com>
2021-08-17 10:46:27 +0530

    3

--- src/lk/ijse/POS/controller/CustomerSaveFormController.java
+++ src/lk/ijse/POS/controller/CustomerSaveFormController.java
@@ -1,4 +1,15 @@
 package lk.ijse.POS.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
 public class CustomerSaveFormController {
+    public TextField txtId;
+    public TextField txtName;
+    public TextField txtAddress;
+    public TextField txtSalary;
+
+    public void saveCustomerOnAction(ActionEvent actionEvent) {
+
+    }
 }
