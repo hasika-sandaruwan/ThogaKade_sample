f4946c124b23fbf04e65bdab005d19f4f957218a
hasika-sandaruwan <hasikasandaruwan.info@gmail.com>
2021-08-17 11:55:40 +0530

    6

--- src/lk/ijse/POS/controller/CustomerController.java
+++ src/lk/ijse/POS/controller/CustomerController.java
@@ -2,33 +2,23 @@ package lk.ijse.POS.controller;
 
 import lk.ijse.POS.model.Customer;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 
 public class CustomerController {
 
     public boolean saveCustomer(Customer c) throws ClassNotFoundException, SQLException {
-        // load Driver
         Class.forName("com.mysql.cj.jdbc.Driver");
-        // create Connection (import java.sql.Connection)
         Connection con = DriverManager.getConnection(
                 "jdbc:mysql://localhost:3306/ThogaKade", "root", "mysql");
-        // create SQL
-        String query = "INSERT INTO Customer VALUES ('" + c.getId() +
-                "','" + c.getName() + "','" + c.getAddress() + "','" + c.getSalary() + "')";
-        // create Statement
-        Statement stm = con.createStatement();
+        String query = "INSERT INTO Customer VALUES (?,?,?,?)";
+        PreparedStatement stm = con.prepareStatement(query);
+        stm.setObject(1,c.getId());
+        stm.setObject(2,c.getName());
+        stm.setObject(3,c.getAddress());
+        stm.setObject(4,c.getSalary());
+        return stm.executeUpdate() > 0;
 
-        // statement execute (INSERT,UPDATE,DELETE[executeUpdate]) (SELECT[executeQuery])
-        return stm.executeUpdate(query)>0;
-        /*return isSaved>0;*/
-        /*if (isSaved>0){
-            return true;
-        }
-        return false;*/
     }
 
     public Customer searchCustomer(String id) {
