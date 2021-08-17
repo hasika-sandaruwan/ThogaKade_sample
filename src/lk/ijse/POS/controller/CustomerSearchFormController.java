383c0c1c01ee15e645b845a882faa9301d00a49b
hasika-sandaruwan <hasikasandaruwan.info@gmail.com>
2021-08-17 12:21:50 +0530

    8

--- src/lk/ijse/POS/controller/CustomerSearchFormController.java
+++ src/lk/ijse/POS/controller/CustomerSearchFormController.java
@@ -1,7 +1,11 @@
 package lk.ijse.POS.controller;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
+import lk.ijse.POS.model.Customer;
+
+import java.sql.SQLException;
 
 public class CustomerSearchFormController {
     public TextField txtId;
@@ -10,6 +14,24 @@ public class CustomerSearchFormController {
     public TextField txtSalary;
 
     public void searchCustomerOnAction(ActionEvent actionEvent) {
+
+        try {
+
+            Customer c1= new CustomerController().searchCustomer(txtId.getText());
+
+            if (c1!=null){
+                txtName.setText(c1.getName());
+                txtAddress.setText(c1.getAddress());
+                txtSalary.setText(String.valueOf(c1.getSalary()));
+            }else{
+                new Alert(Alert.AlertType.WARNING, "Empty Result").show();
+            }
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
 
     }
 }
